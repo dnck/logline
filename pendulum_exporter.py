ba6481ed7aa863051c15e908d248f7373abaab10
dnck <dnck@users.noreply.github.com>
2019-09-25 15:15:32 +0200

    fix mssing node metrics

--- pendulum_exporter.py
+++ pendulum_exporter.py
@@ -183,11 +183,22 @@ def set_class_metric(node_id, metric, val):
                 )
             )
 
+KNOWN_NODES = []
+node_metrics = {}
+
 def export_metrics(exporter_queue):
     while True:
         line = exporter_queue.get(timeout=10000.0)
         if line:
             node = re.match(node_id_pattern, line)
+            if node not in KNOWN_NODES and len(KNOWN_NODES) < 10:
+                KNOWN_NODES.append(node)
+                node_metrics.update(
+                    construct_metrics(
+                        node,
+                        api_request_metrics,
+                        x_class_metrics
+                    )
             if node:
                 node_id = node.group(0)
                 api_request = match_for_api_request(api_request_metrics, line)
