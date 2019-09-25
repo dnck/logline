37c08d024e164b283193333a023832348a4d3a83
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-09-25 17:00:20 +0200

    add feature for docker logs to concatenate node name

--- pipe_log.py
+++ pipe_log.py
@@ -10,26 +10,26 @@ if sys.version_info.major > 2:
 else:
     from Queue import Queue
 
-def broadcast_client(host, port, postdata):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    send_bytes = json.dumps(postdata).encode()
-    #print("Sending lines...")
-    sock.sendto(send_bytes, (host, port))
-
-def send_to_queue(broadcast_queue, lines):
-    broadcast_queue.put(lines)
-
-def get_from_queue(broadcast_queue, host, port):
-    while True:
-        postdata = broadcast_queue.get(timeout=10000.0)
-        if len(postdata):
-            broadcast_client(host, port, postdata)
-
 def trail_log(broadcast_queue):
     for line in sys.stdin:
         lines = line.rstrip()
-        send_to_queue(broadcast_queue, lines)
+        _send_to_queue(broadcast_queue, lines)
+
+def _send_to_queue(broadcast_queue, lines):
+    broadcast_queue.put(lines)
+
+def send_datagram(broadcast_queue, host, port, node_name):
+    while True:
+        postdata = broadcast_queue.get(timeout=10000.0)
+        if len(postdata):
+            _send_datagram(host, port, postdata, node_name)
+
+def _send_datagram(host, port, postdata, node_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    send_bytes = json.dumps(postdata).encode()
+    sock.sendto(send_bytes, (host, port))
+
 
 if __name__ == '__main__':
 
@@ -44,6 +44,10 @@ if __name__ == '__main__':
         metavar='-port', type=int, default=5222,
         help='The log server port where we ship to'
     )
+    PARSER.add_argument('-node_name',
+        metavar='node_name', type=str, default='',
+        help='The name of the node to prepend to the log lines'
+    )
 
     args = PARSER.parse_args()
 
@@ -54,8 +58,8 @@ if __name__ == '__main__':
     )
 
     broadcast_to_receiver_thread = threading.Thread(
-        target=get_from_queue,
-        args = (broadcast_queue, args.host, args.port)
+        target=send_datagram,
+        args = (broadcast_queue, args.host, args.port, args.node_name,)
     )
 
     send_to_queue_thread.start()
