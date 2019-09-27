d74f69690c333982ffb376abfc751cbfd4673396
dnck <dnck@users.noreply.github.com>
2019-09-27 10:49:55 +0200

    option to write name of log

--- receive_log.py
+++ receive_log.py
@@ -5,18 +5,18 @@ import json
 
 MAX_BYTES = 65535
 
-def write_line(line):
-    with open('./logs/test.log', 'a') as f:
+def write_line(line, fname):
+    with open('./logs/{}.log'.format(fname), 'a') as f:
         f.write(line+'\n')
 
-def log_server(host, port):
+def log_server(host, port, fname):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((host, port))
     print('Server listening at {}'.format(sock.getsockname()))
     while True:
         received_bytes, address = sock.recvfrom(MAX_BYTES)
         client_data = json.loads(received_bytes.decode())
-        write_line(client_data)
+        write_line(client_data, fname)
 
 
 if __name__ == '__main__':
@@ -29,10 +29,14 @@ if __name__ == '__main__':
         help='The log server public IP where we listen on'
     )
     PARSER.add_argument('-port',
-        metavar='-port', type=int, default=5222,
+        metavar='port', type=int, default=5222,
         help='The log server port where we listen on'
+    )
+    PARSER.add_argument('-fname',
+        metavar='fname', type=int, default=5222,
+        help='The name of the log to write. Will be written in logs dir.'
     )
 
     args = PARSER.parse_args()
 
-    log_server(args.host, args.port)
+    log_server(args.host, args.port, args.fname)
