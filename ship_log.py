59cb2f257bf6b2e52936057fe8b5b545948c9b44
dnck <dnck@users.noreply.github.com>
2019-09-27 10:58:02 +0200

    fix missing space between nodename for loglines

--- ship_log.py
+++ ship_log.py
@@ -39,7 +39,7 @@ def _send_datagram(host, port, postdata, node_name):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.sendto(
-        json.dumps(node_name+" "+postdata).encode(),
+        json.dumps(node_name+" | "+postdata).encode(),
         (host, port)
     )
 
